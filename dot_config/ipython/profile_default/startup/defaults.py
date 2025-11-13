f7901f0da828d4cae6e4f60cfdd1fe6c2aad032b
Karl Krauth <karl.krauth@gmail.com>
2025-11-13 16:18:53 +0100

    Add aliases and make ipython defaults conditional.

--- dot_config/ipython/profile_default/startup/defaults.py
+++ dot_config/ipython/profile_default/startup/defaults.py
@@ -1,12 +1,23 @@
-from IPython import get_ipython
-import magnify as mg
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import scipy as sp
-import xarray as xr
+import datetime
+import glob
+import math
+import os
+import path
+import re
+import sys
+import time
 
+if os.environ.get("UV_GLOBAL", "0") == "1":
+    import magnify as mg
+    import matplotlib.pyplot as plt
+    import numba
+    import numpy as np
+    import pandas as pd
+    import scipy as sp
+    import tifffile
+    import xarray as xr
+
+from IPython import get_ipython
 ipy = get_ipython()
 ipy.run_line_magic("load_ext", "autoreload")
 ipy.run_line_magic("autoreload", "2")
-
